5459183eac4f62f36c9ba64ecfba0cb3d539cfc7
MHassandevwordpress <hassanusmanportfolio@gmail.com>
2024-08-12 18:22:37 +0500

    updated header code

--- header.php
+++ header.php
@@ -4,42 +4,57 @@
 */
 
 ?>
-<html>
+<!doctype html>
+<html lang="en">
+
 <head>
-  <title>TEK2D</title>
-	<?php wp_head(); ?>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-	</head>
-<body class="bg-color">
-<header>
-    <div class="container">
-      <nav class="navbar navbar-expand-lg custom-navbar">
-        <div class="container-fluid">
-			<div class="tek2d-logo">
-          <a class="navbar-brand" href="https://new.tek2d.com/"><?php the_custom_logo() ?></a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-            aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-		</div>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-  <?php 
-wp_nav_menu(array(
-    'theme_location' => 'primary',
-    'menu_class' => 'navbar-nav me-auto mb-2 mb-lg-0',
-    'add_li_class' => 'nav-item',
-    'add_a_class' => 'nav-link'
-));
-			?>
-            <div class="header-button">
-              <a href="#">Get Started</a>
-            </div>
-          </div>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>TEK2D</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body onload="init()" class="bg-color">
+    <header>
+        <div class="container">
+            <nav class="navbar navbar-expand-lg custom-navbar">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="index.html"><img
+                            src="https://new.tek2d.com/wp-content/uploads/2024/07/ffvLayer_1.png" alt="TEK2D">
+                            <div class="logo-hover-text">
+                                <p>Tek2D</p>
+                            </div>
+                        </a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">HOME</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">About Us</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Servicesk</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Portfolio</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Contact Us</a>
+                            </li>
+                        </ul>
+                        <div class="header-button">
+                            <a href="#">Get Started</a>
+                        </div>
+                    </div>
+                </div>
+            </nav>
         </div>
-      </nav>
-    </div>
-  </header>
+    </header>
